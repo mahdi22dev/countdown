8a5d4709ac4890befdc73073905ba51cedb07c79
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-09-05 10:11:39 +0100

    latest

--- src/components/CountDownForm.jsx
+++ src/components/CountDownForm.jsx
@@ -50,89 +50,92 @@ const CountDownForm = () => {
     >
       <p className='text-red-900 min-h-6'>{}</p>
 
-      {/* form inputs */}
-      <div className=' mb-2'>
-        <div className='mb-1'>
-          <label htmlFor={"title"} className='capitalize text-primary-content'>
-            {"title"}
-          </label>
-        </div>
-        <Controller
-          name={"title"}
-          control={control}
-          render={({ field }) => (
-            <input
-              className='input input-primary w-full rounded-sm'
-              {...field}
-            />
+      <div className='inputs'>
+        {" "}
+        {/* form inputs */}
+        <div className=' mb-2'>
+          <div className='mb-1'>
+            <label
+              htmlFor={"title"}
+              className='capitalize text-primary-content'
+            >
+              {"title"}
+            </label>
+          </div>
+          <Controller
+            name={"title"}
+            control={control}
+            render={({ field }) => (
+              <input
+                className='input input-primary w-full rounded-sm'
+                {...field}
+              />
+            )}
+          />
+          {errors.title && (
+            <p className='text-error mt-3 font-light text-sm'>
+              {errors.title.message}
+            </p>
           )}
-        />
-        {errors.title && (
-          <p className='text-error mt-3 font-light text-sm'>
-            {errors.title.message}
-          </p>
-        )}
-      </div>
-      {/* descreption */}
-      <div className=' mb-2'>
-        <div className='mb-1'>
-          <label
-            htmlFor={"description"}
-            className='capitalize text-primary-content'
-          >
-            {"Descreition"}
-          </label>
         </div>
-        <Controller
-          name={"description"}
-          control={control}
-          render={({ field }) => (
-            <textarea
-              placeholder='describe your countdown'
-              className='textarea textarea-bordered textarea-lg w-full max-w-xs'
-              {...field}
-            ></textarea>
-          )}
-        />
-      </div>
-
-      {/* form inputs */}
-
-      {/* Theme Picker */}
-      <p className='mb-2 text-primary-content'>Pick a Theme</p>
-      <ImageGrid
-        images={images}
-        setSelectedImage={setSelectedImage}
-        selectedImage={selectedImage}
-      />
-      {/* Date Picker */}
-
-      <div className='mb-2 mt-2'>
-        <div className='mb-1'>
-          <label htmlFor={"date"} className='capitalize text-primary-content'>
-            Pick a Date
-          </label>
+        {/* descreption */}
+        <div className=' mb-2'>
+          <div className='mb-1'>
+            <label
+              htmlFor={"description"}
+              className='capitalize text-primary-content'
+            >
+              {"Descreition"}
+            </label>
+          </div>
+          <Controller
+            name={"description"}
+            control={control}
+            render={({ field }) => (
+              <textarea
+                placeholder='describe your countdown'
+                className='textarea textarea-bordered textarea-lg w-full max-w-xs'
+                {...field}
+              ></textarea>
+            )}
+          />
         </div>
-        <Controller
-          name={"date"}
-          control={control}
-          render={({ field }) => (
-            <DatePicker
-              className='h-13 p-2 cursor-pointer input-primary input input-bordered z-50 text-primary '
-              selected={targetDate}
-              onSelect={(date) => setTargetDate(date)}
-              style={{ zIndex: 1000 }}
-              showPopperArrow
-              value={targetDate}
-              {...field}
-            />
-          )}
+        {/* form inputs */}
+        {/* Theme Picker */}
+        <p className='mb-2 text-primary-content'>Pick a Theme</p>
+        <ImageGrid
+          images={images}
+          setSelectedImage={setSelectedImage}
+          selectedImage={selectedImage}
         />
-        {errors.date && (
-          <p className='text-red-500 mt-3 font-light text-sm'>
-            {errors.date.message}
-          </p>
-        )}
+        {/* Date Picker */}
+        <div className='mb-2 mt-2'>
+          <div className='mb-1'>
+            <label htmlFor={"date"} className='capitalize text-primary-content'>
+              Pick a Date
+            </label>
+          </div>
+          <Controller
+            name={"date"}
+            control={control}
+            render={({ field }) => (
+              <DatePicker
+                className='h-13 p-2 cursor-pointer input-primary input input-bordered z-50 text-primary '
+                selected={targetDate}
+                onSelect={(date) => setTargetDate(date)}
+                style={{ zIndex: 1000 }}
+                showPopperArrow
+                value={targetDate}
+                {...field}
+              />
+            )}
+          />
+          {errors.date && (
+            <p className='text-red-500 mt-3 font-light text-sm'>
+              {errors.date.message}
+            </p>
+          )}
+        </div>
       </div>
 
       <button
