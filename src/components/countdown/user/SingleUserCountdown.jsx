2f973053d15032878d945e81b7a3c67e584fb040
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-09-08 21:51:08 +0100

    edit

--- src/components/countdown/user/SingleUserCountdown.jsx
+++ src/components/countdown/user/SingleUserCountdown.jsx
@@ -3,16 +3,13 @@ import CountdownUi from "../general/CountdownUi";
 import Image from "next/image";
 
 const SingleUserCountdown = ({ countdown }) => {
-  console.log(countdown);
   return (
-    <div className='flex flex-col justify-between h-72 border border-primary px-2 py-4 bg-neutral rounded-md relative'>
+    <div className='flex flex-col justify-between w-72 h-72 border border-primary px-2 py-4 bg-neutral rounded-md relative'>
       <Image src={`/themes/${countdown.imageId}.jpg`} fill alt='image' />
-      <div className='flex justify-between w-full z-10'>
-        <p>tags</p>
-        <div>delete icon</div>
+      <div className='flex justify-end w-full z-10'>
+        <div>delete icon+view icon</div>
       </div>
       <p className='my-2 text-center z-10  capitalize text-xs sm:text-2xl '>
-        {" "}
         {countdown.title}
       </p>
       <CountdownUi eventtime={countdown.targetDate} />
