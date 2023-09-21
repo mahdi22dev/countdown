5a6c8b9feef3894f6bbbcd8dfdbd1200a6d5bd19
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-09-21 22:52:36 +0100

    fix

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -7,7 +7,7 @@ const Divider = ({ date }) => {
       <p className='text-primary'>
         <span className='text-white'>Countdown to</span> {date}
       </p>
-      <div className='w-[25%] mx-auto ml-1 h-2 bg-primary'></div>
+      <div className='w-[25%] mx-auto ml-1 h-2 bg-primary '></div>
     </div>
   );
 };
