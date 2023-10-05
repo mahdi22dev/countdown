df2a7d2052b25dd27ca660b5cf384b86cc9750c0
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-10-05 19:37:20 +0100

    change movie size

--- src/app/(all)/movies/page.js
+++ src/app/(all)/movies/page.js
@@ -6,7 +6,7 @@ import { getServerSession } from "next-auth";
 import React from "react";
 export default async function page() {
   const session = await getServerSession(authOptions);
-  const size = 10;
+  const size = 12;
   const type = "movie";
   const date = false;
   const data = await getAllWithType(type, size, 0, date);
