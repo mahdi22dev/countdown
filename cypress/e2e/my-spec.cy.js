a1c1d980453b368775827b6b7b6d240ea6d17d90
mahdi22dev <154707248+somerandomdev21@users.noreply.github.com>
2024-01-03 13:12:59 +0100

    login tests run

--- cypress/e2e/my-spec.cy.js
+++ cypress/e2e/my-spec.cy.js
@@ -5,6 +5,10 @@ describe("countdown login tests", () => {
     cy.visit("http://localhost:3000/auth/login");
     cy.get("input[name=email]").type("test@gmail.com");
     cy.get("input[name=password]").type(`mahdi2019{enter}`);
+
+    // we should be redirected to / home page
+    cy.url().should("include", "/");
+    cy.getCookie("next-auth.csrf-token").should("exist");
     cy.task("log", "This is console log : Navigated to login page");
   });
 });
