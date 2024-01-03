505703e77c6b3cf75216ceeaee8c59d103f3bb80
mahdi22dev <154707248+somerandomdev21@users.noreply.github.com>
2024-01-03 13:08:46 +0100

    name

--- cypress/e2e/my-spec.cy.js
+++ cypress/e2e/my-spec.cy.js
@@ -3,6 +3,8 @@ import "cypress-xpath";
 describe("countdown login tests", () => {
   it("visit url", () => {
     cy.visit("http://localhost:3000/auth/login");
+    cy.get("input[name=email]").type("test@gmail.com");
+    cy.get("input[name=password]").type(`mahdi2019{enter}`);
     cy.task("log", "This is console log : Navigated to login page");
   });
 });
